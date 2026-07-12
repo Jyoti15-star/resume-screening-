a1fe923889977a65e223f7005dae5de6ea9cc4b1
Jyoti15-star <jkantia15@gmail.com>
2026-07-12 04:09:49 +0000

    vectorization improvement

--- resumescreening/src/vectorization.py
+++ resumescreening/src/vectorization.py
@@ -1,53 +1,30 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
+
 # Load preprocessed dataset
-df = pd.read_csv(
-    "resumescreening/data/preprocessed_resume_data.csv"
-)
+df = pd.read_csv("resumescreening/data/preprocessed_resume_data.csv")
 df.fillna("", inplace=True)
-df["combined_text"] = (
-    df["resume_text"] + " " +
-    df["skills_required"]
-)
 
+# Input feature
+X = df["resume_text"]
 
-# Combine resume and required skills
-df["combined_text"] = (
-    df["resume_text"] + " " +
-    df["skills_required"]
-)
+# Target
+y = df["job_position_name"]
 
-
-# Create TF-IDF vectorizer
+# TF-IDF
 tfidf = TfidfVectorizer(
     max_features=5000,
     stop_words="english",
     ngram_range=(1,2)
 )
-print(df["combined_text"].isnull().sum())
-print(df["combined_text"].head())
 
-# Convert text into vectors
-X = tfidf.fit_transform(df["combined_text"])
-
+X_tfidf = tfidf.fit_transform(X)
 
 print("Vectorization Completed!")
-print("Number of resumes:", X.shape[0])
-print("Number of features:", X.shape[1])
+print("Number of resumes:", X_tfidf.shape[0])
+print("Number of features:", X_tfidf.shape[1])
 
-
-# Save vectors
-with open(
-    "resumescreening/data/tfidf_vectors.pkl",
-    "wb"
-) as file:
-    pickle.dump(X, file)
-
-
-# Save vectorizer
-with open(
-    "resumescreening/data/tfidf_vectorizer.pkl",
-    "wb"
-) as file:
+# Save TF-IDF Vectorizer
+with open("resumescreening/data/tfidf_vectorizer.pkl", "wb") as file:
     pickle.dump(tfidf, file)
